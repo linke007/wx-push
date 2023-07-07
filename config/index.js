18d8255fa242cc5341711546e51a8fe1088dc1dc
linke007 <138886773+linke007@users.noreply.github.com>
2023-07-07 21:38:24 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  appid: "wx71941f6106ae1072",
-  appsecret: "b4b2ad8a8d4a94e403831886ac10c646",
-  template_id: "q14lTqsDyK0N-KcZUs8UKHOl3GWn2uErDk-8hbm2ouQ",
+  appid: "wxc71b2102db85fec8",
+  appsecret: "87c907656f69e0e703d3943833851c9a",
+  template_id: "LgrcIi-YnlFxI_p0xYObxZn9KFJAh2Qg5yjLVEDlXI8",
   touser: "oonWJ5slWuS2OrJdgCLM3GmNdxVk",
 
   key: "201c93c2e2b34bdeecdbf9f5265eb84e",
-  city: "邢台",
-  first_date: "2022-11-10",
+  city: "焦作",
+  first_date: "2023-05-20",
 };
